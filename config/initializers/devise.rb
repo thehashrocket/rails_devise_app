bbffd1402191b0875040a8bf8ac7037a161465ec
YOUR NAME <jasshultz@gmail.com>
2016-05-29 15:37:48 -0600

    added facebook login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -23,6 +23,9 @@ Devise.setup do |config|
   # available as additional gems.
   require 'devise/orm/active_record'
 
+  # ==> Add Facebook Login
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
